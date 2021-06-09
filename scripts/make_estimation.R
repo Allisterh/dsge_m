97c83ddb24e941dba2213b22422ac4f4190c304e
Alexander Yeliseyev <34523832+boltikpravyi@users.noreply.github.com>
2021-06-09 23:55:39 +0300

    A script for the estimation of the model

--- scripts/make_estimation.R
+++ scripts/make_estimation.R
@@ -10,7 +10,7 @@ load("data/dsge_q.Rds") # quarterly model
 load("data/dsge_m.Rds") # monthly model
 
 # Set a threshold (for estimation)
-threshold <- "2011-03-01"
+threshold <- "2012-04-01"
 
 # Prepare a dataset for estimation ----------------------------------------
 
@@ -21,7 +21,7 @@ data_raw <- "data/data_dsge_m.xlsx" %>%
 # Create a list w/ standardized datasets used as inputs in models
 data_std <- data_raw %>% 
   dplyr::filter(date <= threshold) %>% 
-  standardize_data(nfcst = 12)
+  standardize_data(nfcst = 6)
 
 # Estimation of auxiliary equation used in a monthly model ----------------
 
@@ -38,7 +38,29 @@ result_m <- kalman_smoother_diag(y = t(data_std$Zm), A = dsge_m$A, C = dsge_m$C,
                                 init_V = dsge_m$init_Vm, mchar = "model",
                                 smpl = 1:nrow(data_std$Zm))
 
-# Restore forecast of unstandardized values
-fcst <- (t(result_m$xsmooth) %*% t(dsge_m$C)) * data_std$SD_m + data_std$MZ_m
-plot(fcst[,2], type = 'l') # plot GDP growth
-GDP_m <- fcst[,2]
+# Restore forecast of unstandardized values (monthly model)
+fcst_m <- (t(result_m$xsmooth) %*% t(dsge_m$C)) * data_std$SD_m + data_std$MZ_m
+
+# Forecast of quarterly model
+result_q <- kalman_smoother_diag(y = t(data_std$Zq), A = dsge_q$A, C = dsge_q$C,
+                                 Q = dsge_q$Q, R = dsge_q$R, init_x = dsge_q$init_Xq,
+                                 init_V = dsge_q$init_Vq, mchar = "model",
+                                 smpl = 1:nrow(data_std$Zm))
+
+# Restore forecast of unstandardized values (quarterly model)
+fcst_q <- (t(result_q$xsmooth) %*% t(dsge_q$C)) * data_std$SD_q + data_std$MZ_q
+
+gdp <- data_raw %>% 
+  dplyr::select(date, dyobs) %>% 
+  .[1:nrow(fcst_m),] %>% 
+  add_column(GDP_m = fcst_m[,2]) %>% 
+  dplyr::filter(month(date) %% 3 == 0) %>% 
+  add_column(GDP_q = fcst_q[,2]) %>% 
+  gather(key = "Indicator", value = "Value", -date) %>% 
+  dplyr::filter((date >= threshold) & (Indicator != "dyobs")) %>% 
+  add_column(fcst_date = rep(as_date(threshold))) %>% 
+  mutate(Indicator = as_factor(Indicator))
+
+gdp %>% 
+  ggplot(mapping = aes(x = date, y = Value, color = Indicator)) + 
+    geom_line()
